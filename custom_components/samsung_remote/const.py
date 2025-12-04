4a747d56fee8aef2377e9f742dec567b3557d2ba
Qlimuli <59221558+Qlimuli@users.noreply.github.com>
2025-12-04 18:29:49 +0100

    Update const.py

--- custom_components/samsung_remote/const.py
+++ custom_components/samsung_remote/const.py
@@ -1,63 +1,68 @@
-"""Constants for Samsung Remote integration."""
+"""Constants for the Samsung Remote integration."""
 
-import logging
-
-# Integration domain
 DOMAIN = "samsung_remote"
 
-# Logger
-LOGGER = logging.getLogger(__package__)
+# Connection methods
+CONNECTION_METHOD_SMARTTHINGS = "smartthings"
+CONNECTION_METHOD_LOCAL = "local"
 
-# Configuration keys
-CONF_API_METHOD = "api_method"
+# SmartThings API
+SMARTTHINGS_API_BASE = "https://api.smartthings.com/v1"
+SMARTTHINGS_DOMAIN = "smartthings"
+
+# Configuration
+CONF_CONNECTION_METHOD = "connection_method"
 CONF_DEVICE_ID = "device_id"
 CONF_DEVICE_NAME = "device_name"
-CONF_SMARTTHINGS_TOKEN = "smartthings_token"
-CONF_LOCAL_IP = "local_ip"
-CONF_LOCAL_PSK = "local_psk"
+CONF_REFRESH_TOKEN = "refresh_token"
+CONF_ACCESS_TOKEN = "access_token"
 
-# Defaults
-DEFAULT_API_METHOD = "smartthings"
-DEFAULT_TIMEOUT = 10
+# Attributes
+ATTR_COMMAND = "command"
+ATTR_DEVICE_ID = "device_id"
 
-# SmartThings commands (limited set supported by SmartThings API)
+# Services
+SERVICE_SEND_COMMAND = "send_command"
+SERVICE_REFRESH_TOKEN = "refresh_oauth_token"
+
+# Default values
+DEFAULT_NAME = "Samsung TV"
+DEFAULT_PORT = 8001
+DEFAULT_TIMEOUT = 3
+
+# Supported commands mapping
 SMARTTHINGS_COMMANDS = {
-    "UP",
-    "DOWN",
-    "LEFT",
-    "RIGHT",
-    "OK",
-    "BACK",
-    "HOME",
-    "MENU",
-    "EXIT",
-    "MUTE",
-    "PLAY",
-    "PAUSE",
-    "STOP",
-    "REWIND",
-    "FF",
-    "PLAY_BACK",
-    "SOURCE",
-}
-
-# Samsung TV key mapping
-SAMSUNG_KEY_MAP = {
-    # Power
-    "POWER": "KEY_POWER",
-    "POWEROFF": "KEY_POWEROFF",
+    # Navigation
+    "UP": "up",
+    "DOWN": "down",
+    "LEFT": "left",
+    "RIGHT": "right",
+    "OK": "select",
+    "ENTER": "select",
+    "BACK": "back",
+    "HOME": "home",
+    "MENU": "menu",
+    "EXIT": "exit",
     
     # Volume
-    "VOLUP": "KEY_VOLUP",
-    "VOLDOWN": "KEY_VOLDOWN",
-    "MUTE": "KEY_MUTE",
+    "MUTE": "mute",
     
-    # Channels
-    "CHUP": "KEY_CHUP",
-    "CHDOWN": "KEY_CHDOWN",
-    "PRECH": "KEY_PRECH",
+    # Playback
+    "PLAY": "play",
+    "PAUSE": "pause",
+    "STOP": "stop",
+    "REWIND": "rewind",
+    "FF": "fastForward",
+    "PLAY_BACK": "rewind",
     
-    # Navigation
+    # Source
+    "SOURCE": "selectInputSource",
+}
+
+# Tizen key mapping (für local connection)
+TIZEN_KEYS = {
+    "POWER": "KEY_POWER",
+    "POWEROFF": "KEY_POWEROFF",
     "UP": "KEY_UP",
     "DOWN": "KEY_DOWN",
     "LEFT": "KEY_LEFT",
@@ -66,9 +71,22 @@ SAMSUNG_KEY_MAP = {
     "ENTER": "KEY_ENTER",
     "RETURN": "KEY_RETURN",
     "BACK": "KEY_RETURN",
-    "EXIT": "KEY_EXIT",
     "HOME": "KEY_HOME",
     "MENU": "KEY_MENU",
+    "GUIDE": "KEY_GUIDE",
+    "INFO": "KEY_INFO",
+    "EXIT": "KEY_EXIT",
+    "SOURCE": "KEY_SOURCE",
+    
+    # Volume
+    "VOLUME_UP": "KEY_VOLUP",
+    "VOLUME_DOWN": "KEY_VOLDOWN",
+    "MUTE": "KEY_MUTE",
+    
+    # Channel
+    "CHANNEL_UP": "KEY_CHUP",
+    "CHANNEL_DOWN": "KEY_CHDOWN",
+    "PRECH": "KEY_PRECH",
     
     # Playback
     "PLAY": "KEY_PLAY",
@@ -76,27 +94,19 @@ SAMSUNG_KEY_MAP = {
     "STOP": "KEY_STOP",
     "REWIND": "KEY_REWIND",
     "FF": "KEY_FF",
-    "PLAY_BACK": "KEY_PLAY_BACK",
-    
-    # Source
-    "SOURCE": "KEY_SOURCE",
-    "HDMI": "KEY_HDMI",
-    "HDMI1": "KEY_HDMI1",
-    "HDMI2": "KEY_HDMI2",
-    "HDMI3": "KEY_HDMI3",
-    "HDMI4": "KEY_HDMI4",
+    "REC": "KEY_REC",
     
     # Numbers
-    "NUM0": "KEY_0",
-    "NUM1": "KEY_1",
-    "NUM2": "KEY_2",
-    "NUM3": "KEY_3",
-    "NUM4": "KEY_4",
-    "NUM5": "KEY_5",
-    "NUM6": "KEY_6",
-    "NUM7": "KEY_7",
-    "NUM8": "KEY_8",
-    "NUM9": "KEY_9",
+    "0": "KEY_0",
+    "1": "KEY_1",
+    "2": "KEY_2",
+    "3": "KEY_3",
+    "4": "KEY_4",
+    "5": "KEY_5",
+    "6": "KEY_6",
+    "7": "KEY_7",
+    "8": "KEY_8",
+    "9": "KEY_9",
     
     # Color buttons
     "RED": "KEY_RED",
@@ -104,21 +114,22 @@ SAMSUNG_KEY_MAP = {
     "YELLOW": "KEY_YELLOW",
     "BLUE": "KEY_BLUE",
     
-    # Additional
-    "GUIDE": "KEY_GUIDE",
-    "CH_LIST": "KEY_CH_LIST",
+    # HDMI
+    "HDMI": "KEY_HDMI",
+    "HDMI1": "KEY_HDMI1",
+    "HDMI2": "KEY_HDMI2",
+    "HDMI3": "KEY_HDMI3",
+    "HDMI4": "KEY_HDMI4",
+    
+    # Special
     "TOOLS": "KEY_TOOLS",
-    "INFO": "KEY_INFO",
-    "PICTURE_MODE": "KEY_PICTURE_MODE",
+    "CH_LIST": "KEY_CH_LIST",
+    "PICTURE_MODE": "KEY_PICTURE_SIZE",
     "SOUND_MODE": "KEY_SOUND_MODE",
     "SLEEP": "KEY_SLEEP",
     "ASPECT": "KEY_ASPECT",
     "CAPTION": "KEY_CAPTION",
     "SETTINGS": "KEY_SETTINGS",
-    "E_MANUAL": "KEY_E_MANUAL",
+    "E_MANUAL": "KEY_CONTENTS",
     "SEARCH": "KEY_SEARCH",
-    "REC": "KEY_REC",
 }
-
-# All supported commands (combination of SmartThings and Tizen)
-SUPPORTED_COMMANDS = list(SAMSUNG_KEY_MAP.keys())
